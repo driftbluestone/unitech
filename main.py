9b0c2dbe0d4c59bcf386c263bd4d702c50c4bb75
driftbluestone <driftbluestone@gmail.com>
2026-06-10 20:41:24 -0700

    galaxy

--- main.py
+++ main.py
@@ -3,32 +3,47 @@ import numpy as np
 import matplotlib.pyplot as plt
 from discord import app_commands
 from discord.ext import commands
-from PIL import Image
+from PIL import Image, ImageDraw
 from pathlib import Path
 DIR = Path(__file__).parent.absolute()
 
-phi = (((5 ** 0.5) + 1) / (4*np.pi))
-def graph_spiral():
+random.seed(0)
+phi = (((5 ** 0.5) + 1) / (2*np.pi))
+def graph_spiral(spirals: int): 
     img = Image.new("RGBA", (2000, 2000), color=(0, 0, 0))
-    im = Image.new("RGBA", (2000, 2000), color=(0, 0, 0, 0))
-    for i in range(4):
-        theta = np.linspace(i*(np.pi/2), (4*np.pi)+(i*(np.pi/2)))
+    plot_gray = Image.new("RGBA", (2000, 2000), color=(0, 0, 0, 0))
+    plot_white= Image.new("RGBA", (2000, 2000), color=(0, 0, 0, 0))
+    for i in range(spirals):
+        theta = np.linspace(i*(2 * (np.pi) / spirals), (2*np.pi)+(i*(2 * (np.pi) / spirals)), 1000)
         r = (np.e ** (phi * theta))
         fig, ax = plt.subplots(subplot_kw={"projection": "polar"})
+        
         ax.spines['polar'].set_color((0, 0, 0, 0))
         ax.patch.set_alpha(0)
         ax.grid(False)
         ax.set_xticklabels([])
         ax.set_yticklabels([])
-        ax.plot(theta, r, color = (1, 1, 1), linewidth = 2)
+
+        ax.plot(theta, r, color = (1, 1, 1), linewidth = 20)
         buffer = io.BytesIO()
         fig.savefig(buffer, dpi = 300, transparent=True)
-        plt.close()
         plot = Image.open(buffer)
-        im.paste(plot, (0, 0), plot)
+        plot_white.paste(plot, (0, 0), plot)
+
+        ax.plot(theta, r, color = (0.5, 0.5, 0.5), linewidth = 40)
+        buffer = io.BytesIO()
+        fig.savefig(buffer, dpi = 300, transparent=True)
+        plot = Image.open(buffer)
+        plot_gray.paste(plot, (0, 0), plot)
+        
+        plt.close()
     
-    im = im.crop(im.getbbox()).resize((2000, 2000))
-    img.paste(im, (0, 0), im)
+    plot_gray = plot_gray.crop(plot_gray.getbbox()).resize((2000, 2000))
+    plot_white = plot_white.crop(plot_white.getbbox()).resize((2000, 2000))
+    
+    img.paste(plot_gray, (0, 0), plot_gray)
+    img.paste(plot_white, (0, 0), plot_white)
+
     image = io.BytesIO()
     img.save(image, "PNG")
     image.seek(0)
@@ -36,7 +51,6 @@ def graph_spiral():
 
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(UniTech(bot=bot))
-random.seed(0)
 
 star_data = {
     "yellow": {
@@ -57,5 +71,10 @@ class UniTech(commands.Cog):
         self.bot = bot
 
     @app_commands.command(name="starmap", description="starmap? maybe!")
-    async def starmap(self, interaction: discord.Interaction):
-        await interaction.response.send_message(file=discord.File(fp=graph_spiral(), filename="dn.png"))
+    async def starmap(self, interaction: discord.Interaction, arms: int):
+        await interaction.response.defer()
+        await interaction.followup.send(file=discord.File(fp=graph_spiral(arms), filename="dn.png"))
+
+if __name__ == "__main__":
+    image = Image.open(graph_spiral(4))
+    image.show()
